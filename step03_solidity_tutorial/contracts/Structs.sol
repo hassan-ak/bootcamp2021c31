b8869cc85b57e23adccfd61eef38ecccd9c6ec21
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-02-15 17:42:16 +0500

    Create Structs.sol

---
+++ step03_solidity_tutorial/contracts/Structs.sol
@@ -0,0 +1,63 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Structs {
+    // ***************************//
+    // ********* Structs *********//
+    // ***************************//
+    /* 
+        Struct types are used to represent a record.
+        The struct keyword defines a new data type, with more than one member
+    */
+
+    /* Example One*/
+    struct Book {
+        string title;
+        string author;
+        uint256 book_id;
+    }
+
+    Book book;
+
+    function setBook() public {
+        book = Book("Learn Java", "TP", 1);
+    }
+
+    function getBookId() public view returns (uint256) {
+        return book.book_id;
+    }
+
+    /* Example Two*/
+    enum FundingRound {
+        SEED,
+        PRIVATE,
+        PUBLIC
+    }
+
+    struct FundingRoundDetails {
+        uint256 fundingRequired;
+        FundingRound round;
+    }
+
+    FundingRoundDetails[] public allRounds;
+
+    function addFundingRound() public {
+        FundingRoundDetails memory details1 = FundingRoundDetails(
+            10000,
+            FundingRound.SEED
+        );
+        FundingRoundDetails memory details2 = FundingRoundDetails(
+            20000,
+            FundingRound.PRIVATE
+        );
+        FundingRoundDetails memory details3 = FundingRoundDetails(
+            20000,
+            FundingRound.PUBLIC
+        );
+        allRounds.push(details1);
+        allRounds.push(details2);
+        allRounds.push(details3);
+    }
+}
