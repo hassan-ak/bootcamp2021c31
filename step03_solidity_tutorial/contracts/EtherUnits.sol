42c702483ee56fdcae4426bde8b6ed2e477a9e29
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-02-16 20:55:16 +0500

    Update EtherUnits.sol

--- step03_solidity_tutorial/contracts/EtherUnits.sol
+++ step03_solidity_tutorial/contracts/EtherUnits.sol
@@ -14,21 +14,25 @@ contract EtherUnits {
         Lowest unit is wei and 1e12.
      */
 
+    /*
     assert(1 wei == 1);
     assert(1 szabo == 1e12);
     assert(1 finney == 1e15);
     assert(1 ether == 1e18);
     assert(2 ether == 2000 fenny);
+    */
 
     // Ether
-    uint256 amount = 1 ether;
-    uint256 amount = 1_000_000_000_000_000_000;
-    uint256 amount = 1e18;
+    uint256 amount1 = 1 ether;
+    uint256 amount2 = 1_000_000_000_000_000_000;
+    uint256 amount3 = 1e18;
 
     // Time
+    /*
     assert(1 seconds == 1);
     assert(1 minutes == 60 seconds);
     assert(1 hours == 60 minutes);
     assert(1 day == 24 hours);
     assert(1 week == 7 days);
+    */
 }
